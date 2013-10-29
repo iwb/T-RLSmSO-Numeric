1d49b08de6622c017fb166ca6da5786317487867
Julius F. Heins <jfheins@gmail.com>
2013-10-29 04:18:01 +0100

    Sysweld Material hinzugefügt, altes Material entfernt (jetzt durch den config struct einstellbar)

--- Matlab_Skripte/cell2poly.m
+++ Matlab_Skripte/cell2poly.m
@@ -1,14 +1,17 @@
-function [ pol ] = cell2poly( cel )
+function [ polys ] = cell2poly( cells )
 %CELL2POLY Summary of this function goes here
 %   Detailed explanation goes here
-	l = length(cel);
+for ii = 1:size(cells, 2)
 	
-	pol = zeros(1, l/2);
+	cel = cells{ii};
+	l = size(cel, 2);
 	
-	for i=1:2:l
-		deg = str2double(cel{i});
-		pol(deg+1) = str2double(cel{i+1});
+	pol = cel{2};
+	
+	for i=3:2:l
+		pol = [pol ' + ' cel{i+1} '*T^' cel{i}];
 	end
-	pol =  fliplr(pol);
+	polys{ii} =  pol;
+end
 end
 
--- Matlab_Skripte/initMaterial_sysweld.m
+++ Matlab_Skripte/initMaterial_sysweld.m
@@ -1,41 +1,29 @@
-function initMaterial_sysweld( model )
+function initMaterial_sysweld(model)
 
-%% Funktionen definieren
-
-
-
-
-
-	model.material.create('mat1');
-	model.material('mat1').name('Edelstahl 1.4301');
-	model.material('mat1').set('family', 'Steel');
+	mat = model.material.create('mat1');
+	mat.name('Edelstahl 1.4301');
+	mat.set('family', 'steel');
 	
-	model.material('mat1').propertyGroup('def').set('thermalconductivity', {'70'});
-	model.material('mat1').propertyGroup('def').set('density', {'7.8'});
-	model.material('mat1').propertyGroup('def').set('heatcapacity', {'1.2'});
-
+	model.material('mat1').propertyGroup('def').set('thermalconductivity', 'k(T[1/K])[W/(m*K)]');
+	mat.propertyGroup('def').set('density', 'rho(T[1/K])[kg/m^3]');
+	mat.propertyGroup('def').set('heatcapacity', 'C(T[1/K])[J/(kg*K)]');
+	
+	
+	model.material('mat1').propertyGroup('def').func.create('k', 'Piecewise');
+	model.material('mat1').propertyGroup('def').func('k').set('funcname', 'k');
+	model.material('mat1').propertyGroup('def').func('k').set('arg', 'T');
+	model.material('mat1').propertyGroup('def').func('k').set('extrap', 'constant');
+	model.material('mat1').propertyGroup('def').func('k').set('pieces', {'45' '293' '-0.2242957 + 0.7605684*T^1 + -0.04007508*T^2 + 0.002181761*T^3 + -1.836024E-5*T^4';'293' '1200' '8.926275 + -2.900987*T^1 + 0.1470793*T^2 + -0.001254897*T^3 + 3.414011E-6*T^4'});
+	
+	model.material('mat1').propertyGroup('def').func.create('rho', 'Piecewise');
+	model.material('mat1').propertyGroup('def').func('rho').set('funcname', 'rho');
+	model.material('mat1').propertyGroup('def').func('rho').set('arg', 'T');
+	model.material('mat1').propertyGroup('def').func('rho').set('extrap', 'constant');
+	model.material('mat1').propertyGroup('def').func('rho').set('pieces', {'93' '1700' '-0.2242957 + 0.7605684*T^1 + -0.04007508*T^2 + 0.002181761*T^3 + -1.836024E-5*T^4'});
+	
+	model.material('mat1').propertyGroup('def').func.create('C', 'Piecewise');
+	model.material('mat1').propertyGroup('def').func('C').set('funcname', 'C');
+	model.material('mat1').propertyGroup('def').func('C').set('arg', 'T');
+	model.material('mat1').propertyGroup('def').func('C').set('extrap', 'constant');
+	model.material('mat1').propertyGroup('def').func('C').set('pieces', {'128' '310' '270.215 + -1.210511*T^1 + 0.02151566*T^2 + -7.511841E-5*T^3 + 8.136796E-8*T^4';'310' '1311' '109.2073 + 2.571775*T^1 + -0.006528099*T^2 + 7.787524E-6*T^3 + -4.167913E-9*T^4 + 8.090613E-13*T^5'});
 end
-
-x = 4:0.1:14;
-poly = cell2poly({'0','4.8035','1','-1.973989','2','0.4334444','3', ...
-  '-0.03143248','4','8.324035E-4'});
-
-plot(x, polyval(poly, x));
-hold all
-
-x = 14:0.1:47;
-poly = cell2poly({'0','-0.2242957','1','0.7605684','2', ...
-  '-0.04007508','3','0.002181761','4','-1.836024E-5'});
-plot(x, polyval(poly, x));
-
-x = 47:0.1:128;
-poly = cell2poly({'0','8.926275','1', ...
-  '-2.900987','2','0.1470793','3','-0.001254897','4','3.414011E-6'});
-
-plot(x, polyval(poly, x));
-
-x = 128:0.1:310;
-poly = cell2poly({'0', ...
-  '270.215','1','-1.210511','2','0.02151566','3','-7.511841E-5','4', ...
-  '8.136796E-8'});
-plot(x, polyval(poly, x));
