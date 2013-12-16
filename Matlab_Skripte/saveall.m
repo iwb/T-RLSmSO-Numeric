7e2675671361d5e94b98767e068d015b32c1ded5
Workstation Poons <poons@iwb.tum.de>
2013-12-16 10:42:36 +0100

    saveall angepasst, ROI clearen

--- Matlab_Skripte/saveall.m
+++ Matlab_Skripte/saveall.m
@@ -1,11 +1,11 @@
 if (strcmp(getenv('COMPUTERNAME'), 'POONS'))
     mphsave(model, 'E:\DA_Heins\transfer\model.mph');
-    clearvars ans model Solver geometry;
+    clearvars ans model Solver geometry cone;
 
     save('E:\DA_Heins\transfer\workspace.mat');  
 else
     mphsave(model, 'C:\Users\Julius\SkyDrive\DA_Transfer\model.mph');
-    clearvars ans model Solver geometry;
+    clearvars ans model Solver geometry cone;
 
     save('C:\Users\Julius\SkyDrive\DA_Transfer\workspace.mat');    
 end
