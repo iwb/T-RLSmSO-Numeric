788c1f1f4b693950e1b82708528d986f42515a1e
Workstation Plattner <plattner@iwb.tum.de>
2014-01-19 13:34:31 +0100

    Bugfix

--- Matlab_Skripte/resume.m
+++ Matlab_Skripte/resume.m
@@ -17,7 +17,7 @@ Solver = model.sol(sprintf('sol_%d', index));
 getNextSolver(0, 0, 0, index);
 getNextSolverMultigrid(0, 0, 0, index);
 
-conecount = model.geom('geom1').feature.size - 4;
+conecount = model.geom('geom1').feature.size - 3;
 updateKeyhole(0, 0, 0, 0, conecount);
 
 if exist(logPath, 'file')
