a5651466634e3457048a5e4b8b75596219c36cbe
Workstation Poons <plattner@iwb.tum.de>
2014-03-03 09:36:34 +0100

    Mehr Timing

--- Matlab_Skripte/updateKeyhole.m
+++ Matlab_Skripte/updateKeyhole.m
@@ -12,8 +12,11 @@ end
 if isempty(maxTag)
 	maxTag = 0;
 end
-
+ankhstart = tic;
 khg = calcKeyhole(config.dis.KeyholeResolution, speed, temp, config);
+ankhtime = toc(ankhstart);
+fprintf('Calculation of the keyhole took %0.1f sec\n', ankhtime);
+
 assignin('base', 'khg', khg);
 CenterArray = khg(2, :);
 % Calculate the differences of the centers
@@ -23,6 +26,8 @@ RatioArray = RadiusArray(2:end) ./ RadiusArray(1:end-1);
 HeightArray = - diff(khg(1, :));
 
 conetags = cell(0);
+
+conestart = tic;
 
 for i = 1:size(CenterArray, 2)-1
 	% This loops over every gap between two circles. Therefore, the top
@@ -64,9 +69,10 @@ end
 for j = i+1 : maxTag
     model.geom('geom1').feature.remove(['econ_' num2str(j)]);
 end
-
 depth = khg(1, i+1);
 
+conetime = toc(conestart);
+fprintf('Conversion to cones took %0.1f sec\n', conetime);
 fprintf('Keyhole was build out of %d elements, %4.0fµm deep.\n', i, -depth * 1e6);
 
 %% ROI Konus anpassen
@@ -83,7 +89,10 @@ newR = max([prevR, maxR1, maxR2]);
 model.param.set('Cyl_r', sprintf('%.12e [m]', newR));
 
 %% Geometrie finalisieren
+buldingstart = tic;
 model.geom('geom1').run; % Damit die Selektion funktioniert...
+buldingtime = toc(buldingstart);
+fprintf('Building geometry took %0.1f sec\n', buldingtime);
 
 if (maxTag == 0)
     model.selection.create('FM_Domain', 'Explicit');
